d6391b4177ef5b61eee219b1f7477c6679f1b5b1
Hilton Rocha <hilton-bruno-santos@hotmail.com>
2017-04-04 11:58:40 -0300

    Ajuste na formatação do código

--- features/formulario.feature
+++ features/formulario.feature
@@ -1,12 +1,14 @@
 # language: pt
 # encoding: utf-8
 
-Funcionalidade: Preencher Formulario StarWars	
+Funcionalidade: Preencher Formulario StarWars
+				Eu como um usuário
+				Quero acessar o site
+				Para que possa responder o formulario StarWars
 
-		@formulario_starwars
-		
-		Cenário: Preencher formulario
-		Dado que eu acesse questionário StarWars
-		E preencha todos os campos corretamente
-		Quando eu clicar em Enviar
-		Então o questionário será finalizado com a mensagem "Obrigado por responder as perguntas! =)"
+@formulario_starwars
+Cenário: Preencher formulario
+	Dado que eu acesse questionário StarWars
+	E preencha todos os campos corretamente
+	Quando eu clicar em Enviar
+	Então o questionário será finalizado com a mensagem "Obrigado por responder as perguntas! =)"
--- features/step_definitions/preencher_formulario.rb
+++ features/step_definitions/preencher_formulario.rb
@@ -1,22 +1,20 @@
 
-formulario = Formulario.new
-Dado(/^que eu acesse questionário StarWars$/) do
 
-  visit 'https://docs.google.com/forms/d/e/1FAIpQLSdeb-VOSrYEMPs1kqNYLEN2y-_9xZUrNqWb_we60tm2GO6f0w/formResponse'
-  formulario.next_button.click
+Dado(/^que eu acesse questionário StarWars$/) do
+  @formulario = Formulario.new
+  @formulario.load
+  @formulario.next_button.click
   sleep 1
 end
 
 Dado(/^preencha todos os campos corretamente$/) do
- formulario.preenche_formulario
-
+  @formulario.preenche_formulario
 end
 
 Quando(/^eu clicar em Enviar$/) do
- 
-  formulario.salvar
+  @formulario.save_btn.click
 end
 
 Então(/^o questionário será finalizado com a mensagem "([^"]*)"$/) do |message|
-  formulario.valida_formulario(message)
+  @formulario.valida_formulario(message)
 end
